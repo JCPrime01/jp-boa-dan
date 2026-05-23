20f23981ccca98c10c2d866a397f67c865cc25e9
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-23 11:47:48 -0300

    chore: update shareCode to 5JFM6PGF26E

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6e81a4di?shareCode=7A3EOJWVIRT&afp=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6e81a4di?shareCode=5JFM6PGF26E&afp=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
