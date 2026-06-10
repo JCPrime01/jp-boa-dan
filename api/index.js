117b40f06f0c7d367b0700497aef03b4e8862f5d
JCPrime01 <vanzokiller@gmail.com>
2026-06-10 10:53:25 -0300

    chore: shareCode H8GCEPIDCPM -> 4P9IDMTX7BB (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6e81a4di?shareCode=H8GCEPIDCPM&afp=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6e81a4di?shareCode=4P9IDMTX7BB&afp=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
